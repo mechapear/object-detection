e9ff7cb042297271ba8c2959f5714c4f08fffc89
mechapear <mechapear@gmail.com>
2024-01-02 16:54:52 +0700

    feat: upload image file and convert to base64

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
+import { ChangeEventHandler, useState } from 'react'
+
 export default function App() {
+  const [image, setImage] = useState<string | null>(null)
+
+  const handleUploadImage: ChangeEventHandler<HTMLInputElement> = async (
+    event,
+  ) => {
+    const imageInput = event.target.files?.[0]
+    if (!imageInput) return
+
+    const base64 = await getBase64String(imageInput)
+    if (!base64) return
+    setImage(base64)
+  }
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-red-500 underline">
-        Hello world!
-      </h1>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <label>Select image: </label>
+        <input type="file" accept="image/*" onChange={handleUploadImage} />
+        <button type="submit">Submit</button>
+      </form>
+      {image && <img src={image} alt="preview" height="100px" width="100px" />}
     </>
   )
+}
+
+function getBase64String(file: File): Promise<string | void> {
+  return new Promise<string>((resolve, reject) => {
+    // create new FileReader
+    const reader = new FileReader()
+    // convert the file to base64 text
+    reader.readAsDataURL(file)
+
+    reader.onloadend = () => {
+      resolve(reader.result as string)
+    }
+
+    reader.onerror = (error) => {
+      reject(error)
+    }
+  })
+    .then((base64) => {
+      console.log('success!\nbase64 string is: ' + base64)
+      return base64
+    })
+    .catch((error) => console.log(error))
 }
