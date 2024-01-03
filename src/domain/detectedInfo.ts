d854e234a6cd5086e6b23b181f7bd2c2e6042cff
mechapear <mechapear@gmail.com>
2024-01-03 17:53:24 +0700

    feat: add objectCategoryConfig

--- src/domain/detectedInfo.ts
+++ src/domain/detectedInfo.ts
@@ -28,3 +28,36 @@ export type DetectedInfo = {
   service_id: string
   detected_objects: DetectedObject[]
 }
+
+export const objectCategoryConfig = {
+  human: {
+    color: '#3747CE',
+  },
+  vehicle: {
+    color: '#289B6B',
+  },
+  object: {
+    color: '#24AFFD',
+  },
+  animal: {
+    color: '#F3A433',
+  },
+  accessory: {
+    color: '#3747CE',
+  },
+  sport: {
+    color: '#FF556E',
+  },
+  kitchenware: {
+    color: '#7C31AD',
+  },
+  food: {
+    color: '#7C31AD',
+  },
+  furniture: {
+    color: '#FC72A4',
+  },
+  electronic: {
+    color: '#FC72A4',
+  },
+} satisfies Record<ObjectCategory, { color: string }>
